7fbc388e6ddd96eac827a0959ffe577c9d2c0549
vijay5454 <vijaythalaiva5454@gmail.com>
2023-06-30 22:08:04 +0530

    Backend URL
    Updated the Backend URL after deploying the server

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,12 +37,15 @@ const Home = () => {
   const fetchPosts = async () => {
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        "https://generative-ai-service.onrender.com/api/v1/post",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
       if (response.ok) {
         const result = await response.json();
         setAllPosts(result.data.reverse());
